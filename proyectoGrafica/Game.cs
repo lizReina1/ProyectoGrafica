ba0d1f3ee182689a1defe97b70370da2593bd352
lreina <lizfernanda.rq@gmail>
2024-03-23 22:39:50 -0400

    first homework

---
+++ proyectoGrafica/Game.cs
@@ -0,0 +1,104 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyectoGrafica
+{
+    public class Game : GameWindow
+
+    {
+        int VertexBufferObject;
+        private int vertexArrayObject;
+
+        Shader shader;
+
+        
+        public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { 
+        
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+             
+            KeyboardState input = Keyboard.GetState();  
+
+            if (input.IsKeyDown(Key.Escape)) //para la ventanda se cierre con el escape
+            {
+                Exit();
+            }
+        }
+
+        //Cualquier código relacionado con la inicialización debe ir aquí.
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            float[] vertices = {
+                -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+                 0.5f, -0.5f, 0.0f, //Bottom-right vertex
+                 0.0f,  0.5f, 0.0f  //Top vertex
+            };
+
+            VertexBufferObject = GL.GenBuffer();
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+        
+            vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(vertexArrayObject);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            shader = new Shader(@"D:\materias\grafica\proyecto\proyectoGrafica\proyectoGrafica\shader\shader.vert", @"D:\materias\grafica\proyecto\proyectoGrafica\proyectoGrafica\shader\shader.frag");
+
+            shader.Use();
+
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            shader.Use();
+
+            // Bind the VAO
+            GL.BindVertexArray(vertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+
+            SwapBuffers();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Width, Height);
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+            GL.UseProgram(0);
+
+            // Delete all the resources.
+            GL.DeleteBuffer(VertexBufferObject);
+            GL.DeleteVertexArray(vertexArrayObject);
+
+            base.OnUnload(e);
+            shader.Dispose();
+        }
+    }
+}
